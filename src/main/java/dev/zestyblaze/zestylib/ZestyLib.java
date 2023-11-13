cf4ca0abc46e05cc54f083da77746ec6c54544e7
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2023-11-13 21:20:11 +0000

    v1.0.0

---
+++ src/main/java/dev/zestyblaze/zestylib/ZestyLib.java
@@ -0,0 +1,16 @@
+package dev.zestyblaze.zestylib;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.world.entity.player.Player;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ZestyLib implements ModInitializer {
+	public static final String MODID = "ZestyLib";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
